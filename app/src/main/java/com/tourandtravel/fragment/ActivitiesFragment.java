1ce8262dae485284951fc190de92c17fe5cd001a
hbenjwal111 <himanshub11@outlook.com>
2017-10-11 13:57:18 +0530

    new commit

--- app/src/main/java/com/tourandtravel/fragment/ActivitiesFragment.java
+++ app/src/main/java/com/tourandtravel/fragment/ActivitiesFragment.java
@@ -2,6 +2,7 @@ package com.tourandtravel.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
@@ -12,14 +13,46 @@ import android.view.ViewGroup;
 
 import com.tourandtravel.R;
 import com.tourandtravel.adapter.ActivitiesAdapter;
+import com.tourandtravel.utils.Constant;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.facebook.FacebookSdk.getApplicationContext;
 
 /**
  * Created by himanshu on 18-09-2017.
  */
 
-public class ActivitiesFragment extends Fragment {
+public class ActivitiesFragment  extends Fragment {
 
-    public ActivitiesFragment(){
+
+    private RecyclerView recyclerView;
+    private List<Constant> cartList;
+    private ActivitiesAdapter mAdapter;
+
+
+    public static final String[] titles = new String[] { "jeep",
+            "mountin", "trek", "yog"/*,"Chakrata","Kausani"," Chamoli","Dehradun","Dhanaulti","Haridwar",
+
+    "HemkundShahib","Karanprayag"*/};
+
+    public static final String[] descriptions = new String[] {
+            "It is an aggregate accessory fruit",
+            "It is the largest herbaceous flowering plant", "Citrus Fruit",
+            "Mixed Fruits","kkkkkkkkkkkk",",,,,,,hhhhhhh","jjjjj","hhhhhhhhhhhhhhhhhhh","hhhhhhhhhhhh","gggggggg" };
+
+    public static final Integer[] images = { R.drawable.jeep,
+            R.drawable.mountion, R.drawable.trek, R.drawable.yog,R.drawable.chkrt,R.drawable.ksuni
+            ,R.drawable.chmoli,R.drawable.dehrdun,R.drawable.dhnulti,R.drawable.hridwr,R.drawable.hemkund,};
+
+
+
+
+    private FragmentManager fragmentManager;
+
+
+    public  ActivitiesFragment(){
 
 
     }
@@ -28,25 +61,33 @@ public class ActivitiesFragment extends Fragment {
         // Defines the xml file for the fragment
         View rootView = inflater.inflate(R.layout.fragment_activities, parent, false);
 
-        RecyclerView rv = (RecyclerView) rootView.findViewById(R.id.rv_recycler_view1);
+        recyclerView = (RecyclerView) rootView.findViewById(R.id.rv_recycler_view1);
         setHasOptionsMenu(true);
 
+        cartList = new ArrayList<Constant>();
+        for (int i = 0; i < titles.length; i++) {
+            Constant item = new Constant(images[i], titles[i], descriptions[i]);
+            cartList.add(item);
+        }
 
-        LinearLayoutManager llm = new LinearLayoutManager(getActivity());
-        rv.setLayoutManager(llm);
-        getActivity().setTitle("Activities");
+       /* cartList = new ArrayList<>();*/
 
-
-        rv.setHasFixedSize(true);
-        RecyclerView.LayoutManager mLayoutManager2 = new LinearLayoutManager(getActivity());
-        rv.setLayoutManager(mLayoutManager2);
-        rv.addItemDecoration(new DividerItemDecoration(getActivity(), LinearLayoutManager.VERTICAL));
-        rv.setItemAnimator(new DefaultItemAnimator());
-      ActivitiesAdapter adapter = new ActivitiesAdapter(new String[]{"test one", "test two", "test three", "test four", "test five" , "test six" , "test seven"});
-        rv.setAdapter(adapter);
+        mAdapter = new ActivitiesAdapter(getActivity(), cartList);
 
 
+
+
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+
+        recyclerView.addItemDecoration(new DividerItemDecoration(getActivity(), DividerItemDecoration.VERTICAL));
+        recyclerView.setAdapter(mAdapter);
         return rootView;
     }
 
+
+
 }
+
