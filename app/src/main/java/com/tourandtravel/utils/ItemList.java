e465e6614ae9d5550be788f25d6ca25fab94a2f4
hbenjwal111 <himanshub11@outlook.com>
2017-10-11 14:00:02 +0530

    new commit

---
+++ app/src/main/java/com/tourandtravel/utils/ItemList.java
@@ -0,0 +1,23 @@
+package com.tourandtravel.utils;
+
+/**
+ * Created by himanshu on 11-10-2017.
+ */
+
+public class ItemList{
+
+    private int imageId;
+
+
+        public ItemList(int imageId) {
+            this.imageId = imageId;
+
+        }
+        public int getImageId() {
+            return imageId;
+        }
+        public void setImageId(int imageId) {
+            this.imageId = imageId;
+        }
+
+}
