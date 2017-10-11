8faea571f8c41eed26db0e372de63c1a04915936
hbenjwal111 <himanshub11@outlook.com>
2017-10-11 13:56:54 +0530

    new commit

---
+++ app/src/main/java/com/tourandtravel/fragment/AboutFragment.java
@@ -0,0 +1,50 @@
+package com.tourandtravel.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.tourandtravel.R;
+
+/**
+ * Created by himanshu on 11-10-2017.
+ */
+
+public class AboutFragment extends Fragment {
+
+
+
+    public AboutFragment(){
+
+
+    }
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+        // Defines the xml file for the fragment
+        View rootView = inflater.inflate(R.layout.fragment_about, parent, false);
+
+/*
+        RecyclerView rv = (RecyclerView) rootView.findViewById(R.id.rv_recycler_view1);
+*/
+        setHasOptionsMenu(true);
+   /*     ((AppCompatActivity) getActivity()).getSupportActionBar().hide();
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+        return rootView;
+    }
+
+
+}
